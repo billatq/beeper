d8dd79bbcb6e3cf6dde013245e2a7077bd0dfbb5
William Reading <8d8k@anmt.net>
2012-03-10 06:36:24 -0800

    Add preferences

--- src/com/aggienerds/beeper/BeeperActivity.java
+++ src/com/aggienerds/beeper/BeeperActivity.java
@@ -1,24 +1,13 @@
 package com.aggienerds.beeper;
 
-import java.util.List;
-
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceActivity;
 
-public class BeeperActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        MessageRetriever mr = new MessageRetriever(this.getApplicationContext());
-        List<Message> messages = mr.getConversationsLast(true);
-        
-        for (Message m : messages)
-        {
-        	Log.d("BeeperActivity", m.getBody());
-        }
-    }
+public class BeeperActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getPreferenceManager().setSharedPreferencesName("preferences");
+		addPreferencesFromResource(R.xml.preferences);
+	}
 }
