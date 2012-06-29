6af77590b49098e7240fdf9fbe420ed5f7c61b2e
William Reading <8d8k@anmt.net>
2012-06-28 21:55:58 -0700

    Remove unused fields

--- src/com/aggienerds/beeper/Message.java
+++ src/com/aggienerds/beeper/Message.java
@@ -20,16 +20,11 @@ import java.util.Date;
 
 public class Message {
     private String address;
-    private Integer id;
-    private Integer thread_id;
-    private Date date;
     private String subject;
     private String body;
 
     public Message() {
         setAddress(new String());
-        setId(new Integer(-1));
-        setThread_id(new Integer(-1));
         setSubject(new String());
         setBody(new String());
     }
@@ -40,22 +35,6 @@ public class Message {
 
     public void setAddress(String address) {
         this.address = address;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getThread_id() {
-        return thread_id;
-    }
-
-    public void setThread_id(Integer thread_id) {
-        this.thread_id = thread_id;
     }
 
     public String getSubject() {
@@ -72,19 +51,5 @@ public class Message {
 
     public void setBody(String body) {
         this.body = body;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public void setDate(long milliseconds) {
-        Date d = new Date();
-        d.setTime(milliseconds);
-        this.date = d;
     }
 }
